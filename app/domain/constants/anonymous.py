ae2e12510bb9ac28e2ab96ed3262ef519efea0be
y-kawakami <kawakami@kkcraft.co.jp>
2025-02-21 10:48:45 +0900

    匿名の時は null を返す

--- app/domain/constants/anonymous.py
+++ app/domain/constants/anonymous.py
@@ -1,6 +1,9 @@
+from typing import Optional
+
 ANONYMOUS_LABEL = 'Anonymous'
-ANONYMOUS_DISPLAY_NAME = '匿名'
+# ANONYMOUS_DISPLAY_NAME = '匿名'
+ANONYMOUS_DISPLAY_NAME = None
 
 
-def filter_anonymous(nickname: str) -> str:
+def filter_anonymous(nickname: str) -> Optional[str]:
     return ANONYMOUS_DISPLAY_NAME if nickname == ANONYMOUS_LABEL else nickname
